e1bfdf663e738b3d62ecde628fbc992712259620
Bayan Rub <bayanaboalrob2001@gmail.com>
2024-05-23 13:04:00 +0300

    Removing unnecessary usings

--- JsonParser.cs
+++ JsonParser.cs
@@ -1,11 +1,4 @@
 ﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
 namespace WeatherMonitoringApp
 {
     internal class JsonParser : IWeatherDataParser
